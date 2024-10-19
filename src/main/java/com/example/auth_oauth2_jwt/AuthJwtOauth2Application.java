ba438727f5672abf6ccba11969b990f7b4b2541a
jiyong.yoon <jyyoon0615@naver.com>
2024-10-20 02:28:45 +0900

    git init

---
+++ src/main/java/com/example/auth_oauth2_jwt/AuthJwtOauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.auth_oauth2_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthJwtOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthJwtOauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/auth_oauth2_jwt/AuthJwtOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth_oauth2_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthJwtOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
